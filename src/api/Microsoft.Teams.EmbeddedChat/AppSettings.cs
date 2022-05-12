874b29e6b4d4b498dfd33b9116e404a2f405bbd2
Danny Garber <dannyg@outlook.com>
2022-05-12 19:38:23 -0400

    removed obsolete ACS Access Key

--- src/api/Microsoft.Teams.EmbeddedChat/AppSettings.cs
+++ src/api/Microsoft.Teams.EmbeddedChat/AppSettings.cs
@@ -5,7 +5,6 @@ namespace Microsoft.Teams.EmbeddedChat
     {
         public string StorageConnectionString { get; set; }
         public string AcsEndpoint { get; set; }
-        public string AcsAccessKey { get; set; }
         public string AzureTableName { get; set; }
         public string TenantId { get; set; }
     }
