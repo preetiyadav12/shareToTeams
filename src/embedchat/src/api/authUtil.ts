4af3c9d67b10aaf85cd285c909fe9dff58e551e6
Danny Garber <dannyg@outlook.com>
2022-05-12 02:42:18 -0400

    fixed auth; added reading env vars in webpack

--- src/embedchat/src/api/authUtil.ts
+++ src/embedchat/src/api/authUtil.ts
@@ -3,49 +3,53 @@ import { AppSettings } from "../config/appSettings";
 
 export class AuthUtil {
   public static async acquireToken(element: Element, config: AppSettings): Promise<AccessToken | undefined> {
-    console.log("Trying silenty acquire token.");
     // first try silent auth
     return new Promise((resolve, reject) => {
-      this.acquireTokenSilent(element, config).then((token) => {
-        console.log("Token is obtained!");
+      this.acquireTokenSilent(element, config).then((token: AccessToken) => {
         if (token) resolve(token);
         else {
-          console.log("Cannot obtain silently access token. Requires an interactive login");
           // requires an interactive login
           const btn = document.createElement("button");
           btn.innerText = "Sign-in";
           btn.addEventListener("click", () => {
+            // launch popup
             let popupRef = window.open(
-              `https://${config.hostDomain}/auth.html?mode=interactive`,
+              `https://${config.hostDomain}/auth.html?mode=interactive&client_id=${config.clientId}&host_uri=${config.hostDomain}&tenant=${config.tenant}`,
               "Teams Embed",
               "width=700,height=700,toolbar=yes",
             );
+
+            // listen for response
             window.addEventListener("message", (e) => {
               element.removeChild(btn); //TODO: check if successful
-              popupRef?.close();
+              (popupRef as Window).close();
               popupRef = null;
               resolve(e.data);
             });
-            const t = setInterval(() => {
+
+            // set timer in case they close the window
+            const timer = setInterval(() => {
               if (popupRef && popupRef.closed) {
-                clearInterval(t);
-                resolve(undefined);
+                clearInterval(timer);
+                reject();
               }
             }, 500);
           });
+
+          // add button to the DOM
           element.append(btn);
-          console.log(`Created a Sign In button in https://${config.hostDomain}/auth.html`);
         }
       });
-      console.log(`Updated HTML Element: ${element.innerHTML}`);
     });
   }
 
-  private static async acquireTokenSilent(element: Element, config: AppSettings): Promise<any> {
-    console.log(`Creating iFrame for https://${config.hostDomain}/auth.html`);
-    return new Promise((resolve, reject) => {
+  private static async acquireTokenSilent(element: Element, config: AppSettings): Promise<AccessToken> {
+    return new Promise((resolve) => {
       const loginframe = document.createElement("iframe");
-      loginframe.setAttribute("src", `https://${config.hostDomain}/auth.html`);
+      loginframe.setAttribute(
+        "src",
+        `https://${config.hostDomain}/auth.html?client_id=${config.clientId}&host_uri=${config.hostDomain}&tenant=${config.tenant}`,
+      );
       loginframe.style.display = "none";
       element.append(loginframe);
       window.addEventListener("message", (event) => {
