78335438293746cb0376f95c563e16f9f351363f
Manish Saluja <masaluja@microsoft.com>
2022-05-18 22:22:32 -0700

    code for onlinemeetings

--- src/api/Microsoft.Teams.EmbeddedChat/Activities/CreateOnlineMeetingActivity.cs
+++ src/api/Microsoft.Teams.EmbeddedChat/Activities/CreateOnlineMeetingActivity.cs
@@ -1,20 +1,27 @@
-﻿using Azure.Communication.Identity;
-using Microsoft.Azure.WebJobs;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.Teams.EmbeddedChat.ACS;
+using Microsoft.Graph;
+using System.Threading.Tasks;
 using Microsoft.Teams.EmbeddedChat.Models;
+using Microsoft.Teams.EmbeddedChat.Services;
+using System;
+using System.Collections.Generic;
+
 
 namespace Microsoft.Teams.EmbeddedChat.Activities
 {
     public class CreateOnlineMeetingActivity
     {
+        
         private readonly AppSettings _appConfiguration;
 
-        public CreateOnlineMeetingActivity(IOptions<AppSettings> configuration)
+        public CreateOnlineMeetingActivity(IOptions<AppSettings> configuration, IDurableActivityContext context, ILogger log)
         {
             _appConfiguration = configuration.Value;
+
+
         }
 
         /// <summary>
@@ -24,15 +31,78 @@ namespace Microsoft.Teams.EmbeddedChat.Activities
         /// <param name="log"></param>
         /// <returns></returns>
         [FunctionName(Constants.CreateOnlineMeetingActivity)]
-        public string CreateOnlineMeetingAsync(
+        public async Task<String> CreateOnlineMeetingAsync(
             [ActivityTrigger] IDurableActivityContext context, ILogger log)
         {
+
             // retrieves the entity state from the orchestration
-            var entityState = context.GetInput<EntityState>();
+            var requestData = context.GetInput<EntityState>();
 
-            log.LogInformation($"Activity {Constants.UpdateParticipantsActivity} has started.");
+            var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+            var clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+            var tenant = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
 
-            return "";
+            log.LogInformation($"Client Id: {clientId}");
+            log.LogInformation($"Tenant Id: {tenant}");
+
+            if (String.IsNullOrEmpty(clientId))
+            {
+                log.LogError("Missing AZURE_CLIENT_ID environment variable");
+                throw new ArgumentException("Missing AZURE_CLIENT_ID environment variable");
+            }
+            if (String.IsNullOrEmpty(clientSecret))
+            {
+                log.LogError("Missing AZURE_CLIENT_SECRET environment variable");
+                throw new ArgumentException("Missing AZURE_CLIENT_SECRET environment variable");
+            }
+            if (String.IsNullOrEmpty(tenant))
+            {
+                log.LogError("Missing AZURE_TENANT_ID environment variable");
+                throw new ArgumentException("Missing AZURE_TENANT_ID environment variable");
+            }
+
+            var graphService = new GraphService(clientId, clientSecret, tenant);
+
+            GraphServiceClient graphClient = graphService.GetGraphServiceClient();
+
+            var subject = requestData.EntityId;
+            var externalId = Guid.NewGuid();
+
+            List<MeetingParticipantInfo> AttendeesList = new List<MeetingParticipantInfo>();
+            
+            for (int i = 0; i < requestData.Participants.Length; i++)
+            {
+                MeetingParticipantInfo member = new MeetingParticipantInfo
+                {
+                    Identity = new IdentitySet
+                    {
+                        User = new Graph.Identity
+                        {
+                            Id = requestData.Participants[i].Id
+
+                        }
+
+                    }
+
+                };
+                AttendeesList.Add(member);
+
+            }
+
+            var participants = new MeetingParticipants
+            {
+                Attendees = AttendeesList
+            };
+
+            var meetingResponse = await graphClient.Me.OnlineMeetings
+               .CreateOrGet(externalId.ToString(), null, null, participants, null, subject)
+               .Request()
+               .PostAsync();
+
+            var chatThreadId = meetingResponse.ChatInfo.ThreadId;
+            log.LogInformation($"Activity {Constants.CreateOnlineMeetingActivity} has started.");
+            return (chatThreadId);
         }
     }
+
 }
---
+++ src/api/Microsoft.Teams.EmbeddedChat/Services/GraphService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+
+namespace Microsoft.Teams.EmbeddedChat.Services
+{
+    public class GraphService
+    {
+        private readonly GraphServiceClient _graphServiceClient;
+        /// <summary>
+        /// Constructor: create graph service client 
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <param name="clientSecret"></param>
+        /// /// <param name="tenantId"></param>
+        public GraphService(string clientId, string clientSecret, string tenantId)
+        {
+            // The client credentials flow requires that you request the
+            // /.default scope, and preconfigure your permissions on the
+            // app registration in Azure. An administrator must grant consent
+            // to those permissions beforehand.
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            // Multi-tenant apps can use "common",
+            // single-tenant apps must use the tenant ID from the Azure portal
+            // using Azure.Identity;
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId, clientId, clientSecret, options);
+
+            _graphServiceClient = new GraphServiceClient(clientSecretCredential, scopes);
+        }
+        /// <summary>
+        /// create GraphServiceClient
+        /// </summary>
+        /// <returns>GraphServiceClient</returns>
+        public GraphServiceClient GetGraphServiceClient()
+        {
+            return _graphServiceClient;
+        }
+
+
+
+    }
+
+}
