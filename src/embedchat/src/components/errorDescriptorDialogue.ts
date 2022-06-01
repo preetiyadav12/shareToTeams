3dc89e1faaab3aa3bf0964bee1c736777b56d2d4
Preeti Yadav <preetiyadav@PREETIs-MacBook-Pro-2.local>
2022-06-01 13:55:00 +0100

    error handling scenarios for embedchat.ts

--- src/embedchat/src/components/errorDescriptorDialogue.ts
+++ src/embedchat/src/components/errorDescriptorDialogue.ts
@@ -1,20 +1,30 @@
 const template = document.createElement("template");
 template.innerHTML = `<div class="error-container">
 <div class="error-descriptor">
+<span class="error-txt"></span>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
+      Close
+</button>
 </div>
 </div>`;
 
 export class ErrorDescriptorDialogue extends HTMLElement {
-    private errorText:string;
-    constructor(errorText: string)  {
+    constructor()  {
         super();
-        this.errorText = errorText;
         this.render();
+    }
+
+    printError = (errorText: string) => {
+      // var btn = document.createElement("button");
+      // btn.innerText = ;
+      (<HTMLElement>this.querySelector(".error-txt")).innerText = errorText;
     }
 
     render = () => {
         const dom = <HTMLElement>template.content.cloneNode(true);
-        (<HTMLElement>this.querySelector(".error-descriptor")).innerText = this.errorText;
+        (<HTMLElement>dom.querySelector(".navbar-toggler")).addEventListener('click', () => {
+          (<HTMLElement>this.querySelector(".error-container")).style.display = "none";
+        });
         this.appendChild(dom);
     }
 }
